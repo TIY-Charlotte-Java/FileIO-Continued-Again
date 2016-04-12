4d5b10645ed536f6ec3e22378046d40a32703830
Michael Grill <Ultramar@michaels-mbp.apollosrc.root>
2016-04-12 15:09:35 -0400

    TheBeginnings

--- src/com/theironyard/clt/Main.java
+++ src/com/theironyard/clt/Main.java
@@ -6,10 +6,21 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-	//write code here
+	    //write code here
+        //read countries.txt
+        //parse it
+        //set values as countries that start with same letter
+        //ask user for letter
+        //write file for entry
+        //save to file
+        //end
 
         HashMap<String, ArrayList<Country>> map = new HashMap<>();
 
-        
+
+
+
+
+        Country c = new Country();
     }
 }
